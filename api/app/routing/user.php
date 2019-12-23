a5a560d8404f5c57b7269752a414e8dff5e87b82
NaufalAshaW <naufal.asha@gmail.com>
2019-12-23 20:30:20 +0700

    Edit user, belum beres, banyak error

--- api/app/routing/user.php
+++ api/app/routing/user.php
@@ -3,6 +3,7 @@ use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Slim\Routing\RouteCollectorProxy;
 
+//Tampil semua user
 $route->get('/user', function(Request $request, Response $response) {
 
     $query = $this->get('db')->prepare("SELECT * FROM users");
@@ -22,6 +23,7 @@ $route->get('/user', function(Request $request, Response $response) {
 
 });
 
+//Tampil user berdasarkan username
 $route->get('/user/{username}', function(Request $request, Response $response, $args) {
 
     $query = $this->get('db')->prepare("SELECT * FROM users WHERE username=?");
@@ -47,8 +49,71 @@ $route->get('/user/{username}', function(Request $request, Response $response, $
         
 });
 
-$route->post('/user', function(Request $request, Response $response) {
+//Edit user
 
+$route->put('/user/{username}', function(Request $request, Response $response, $args) {
+
+    $query = "UPDATE users SET username=:username, email=:email, password=:password, phone=:phone, avatar=:avatar";
+    $input = $request->getParsedBody();
+
+    if (!empty(trim($input['password']))) {
+        $query .= ", password=:password";
+    }
+
+    if (!empty(trim($input['avatar']))) {
+        $query .= ", avatar=:avatar";
+    }
+
+    $query = $this->get('db')->prepare($query . "WHERE username = :where_username");
+    $query->bindParam('username', $input['username']);
+    $query-> bindParam('email', $input['email']);
+    
+    if (!empty(trim($input['password']))) {
+        $query = bindParam('password', password_hash($input['password'], PASSWORD_DEFAULT));
+    }
+
+    if (!empty(trim($input['avatar']))) {
+        $query = bindParam('avatar',($input['avatar']));
+    }
+
+    $query->bindParam('where_username', $input['username']);
+    $query->bindParam(':avatar',$input['password']);
+    $query->bindParam(':password',$input['password']);
+    $query->bindParam('email',$input['email']);
+
+    // $query = $this->get('db')->prepare("UPDATE users SET username=?, email=?, name=?, password=?, store_name=?,
+    // phone=?, avatar=? WHERE username = ?");
+
+    // $query->bindParam(1, $input['username']);
+    // $query->bindParam(2, $input['email']);
+    // $query->bindParam(3, $input['name']);
+    // $query->bindParam(4, $password);
+    // $query->bindParam(5, $input['store_name']);
+    // $query->bindParam(6, $input['phone']);
+    // $query->bindParam(7, $input['avatar']);
+    // $query->bindParam(8, $args['username']);
+
+    $user = $query->execute();
+
+    $result = [
+        'status' => $user,
+        'data' => []
+    ];
+
+    if($user)
+        $result['data']['message'] = 'User ' . $args['username'] . ' berhasil diperbarui';
+    else
+        $result['data']['message'] = 'Username tidak ada';
+
+    $response->getBody()->write(json_encode($result));
+
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+        
+});
+
+//Tambah user
+$route->post('/user', function(Request $request, Response $response) {
     $input = $request->getParsedBody();
     $password = password_hash($input['password'], PASSWORD_DEFAULT);
 
@@ -80,6 +145,7 @@ $route->post('/user', function(Request $request, Response $response) {
         
 });
 
+//Hapus user
 $route->delete('/user/{username}', function(Request $request, Response $response, $args) {
 
     $query = $this->get('db')->prepare("DELETE FROM users WHERE username=?");
