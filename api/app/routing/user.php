41c0474fba603b10293ecb8a60cf4e2321f3232e
Agus Awaludin <aogust252000@gmail.com>
2019-12-23 14:53:45 +0700

    Delete unused codes

--- api/app/routing/user.php
+++ api/app/routing/user.php
@@ -22,61 +22,6 @@ $route->get('/user', function(Request $request, Response $response) {
 
 });
 
-// ini teh jang edit
-$route->get('/user', function(Request $request, Response $response) {
-
-    $query = $this->get('db')->prepare("SELECT * FROM users");
-    $query->execute();
-
-    $users = $query->fetchAll(PDO::FETCH_OBJ);
-
-    $results = [
-        'status' => true,
-        'data' => $users
-    ];
-
-    $response->getBody()->write(json_encode($results));
-
-    return $response
-        ->withHeader('Content-Type', 'application/json');
-
-});
-// end ini teh jang edit
-
-// edit user
-$route->post('/user/{username}', function(Request $request, Response $response, $args) {
-
-    $input = $request->getParsedBody();
-    $password = password_hash($input['password'], PASSWORD_DEFAULT);
-
-    $query = $this->get('db')->prepare("INSERT INTO users (username, email, name, password, store_name, phone, avatar) VALUES (?,?,?,?,?,?,?)");
-    $query->bindParam(1, $input['username']);
-    $query->bindParam(2, $input['email']);
-    $query->bindParam(3, $input['name']);
-    $query->bindParam(4, $password);
-    $query->bindParam(5, $input['store_name']);
-    $query->bindParam(6, $input['phone']);
-    $query->bindParam(7, $input['avatar']);
-
-    $user = $query->execute();
-
-    $result = [
-        'status' => $user,
-        'data' => []
-    ];
-
-    if($user)
-        $result['data']['message'] = 'User ' . $input['username'] . ' berhasil ditambahkan';
-    else
-        $result['data']['message'] = 'Gagal menambahkan user karena username atau email sudah digunakan';
-
-    $response->getBody()->write(json_encode($result));
-
-    return $response
-        ->withHeader('Content-Type', 'application/json');
-        
-});
-
 $route->get('/user/{username}', function(Request $request, Response $response, $args) {
 
     $query = $this->get('db')->prepare("SELECT * FROM users WHERE username=?");
