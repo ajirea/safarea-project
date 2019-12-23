c2ababd94364c36681d2b7d843ed5d8dd9bc5800
Satria Aji Putra <satmaxt@outlook.com>
2019-12-23 08:51:32 +0700

    Added user edit API

--- api/app/routing/user.php
+++ api/app/routing/user.php
@@ -8,9 +8,14 @@ $route->get('/user', function(Request $request, Response $response) {
     $query = $this->get('db')->prepare("SELECT * FROM users");
     $query->execute();
 
-    $users = $query->fetchAll();
+    $users = $query->fetchAll(PDO::FETCH_OBJ);
 
-    $response->getBody()->write(json_encode($users));
+    $results = [
+        'status' => true,
+        'data' => $users
+    ];
+
+    $response->getBody()->write(json_encode($results));
 
     return $response
         ->withHeader('Content-Type', 'application/json');
@@ -23,7 +28,9 @@ $route->get('/user/{username}', function(Request $request, Response $response, $
     $query->bindParam(1, $args['username']);
     $query->execute();
 
-    $user = $query->fetch();
+    $user = $query->fetch(PDO::FETCH_OBJ);
+
+    // dd($user);
 
     $result = [
         'status' => !$user ? false : true,
@@ -34,6 +41,39 @@ $route->get('/user/{username}', function(Request $request, Response $response, $
         $result['data'] = $user;
     else
         $result['data']['message'] = 'User tidak ditemukan';
+
+    $response->getBody()->write(json_encode($result));
+
+    return $response
+        ->withHeader('Content-Type', 'application/json');
+        
+});
+
+$route->post('/user', function(Request $request, Response $response) {
+
+    $input = $request->getParsedBody();
+    $password = password_hash($input['password'], PASSWORD_DEFAULT);
+
+    $query = $this->get('db')->prepare("INSERT INTO users (username, email, name, password, store_name, phone, avatar) VALUES (?,?,?,?,?,?,?)");
+    $query->bindParam(1, $input['username']);
+    $query->bindParam(2, $input['email']);
+    $query->bindParam(3, $input['name']);
+    $query->bindParam(4, $password);
+    $query->bindParam(5, $input['store_name']);
+    $query->bindParam(6, $input['phone']);
+    $query->bindParam(7, $input['avatar']);
+
+    $user = $query->execute();
+
+    $result = [
+        'status' => $user,
+        'data' => []
+    ];
+
+    if($user)
+        $result['data']['message'] = 'User ' . $input['username'] . ' berhasil ditambahkan';
+    else
+        $result['data']['message'] = 'Gagal menambahkan user karena username atau email sudah digunakan';
 
     $response->getBody()->write(json_encode($result));
 
