0f3f73e7761c241eb2523fde44cb32a4cd4a017b
Satria Aji Putra <satmaxt@outlook.com>
2019-12-23 08:16:06 +0700

    Added user API

--- api/app/routes.php
+++ api/app/routes.php
@@ -9,7 +9,49 @@ $app->get('/', function (Request $request, Response $response, $args) {
 });
 
 $app->group('/api', function(RouteCollectorProxy $route) {
+
+	$route->get('', function (Request $request, Response $response, $args) {
+	    $response->getBody()->write("API");
+	    return $response;
+	});
+
     $route->get('/user', function(Request $request, Response $response) {
-        return $this->get('view')->render($response, 'layouts/app.twig');
+
+    	$query = $this->get('db')->prepare("SELECT * FROM users");
+    	$query->execute();
+
+    	$users = $query->fetchAll();
+
+    	$response->getBody()->write(json_encode($users));
+
+        return $response
+        	->withHeader('Content-Type', 'application/json');
+
     });
+
+    $route->get('/user/{username}', function(Request $request, Response $response, $args) {
+
+    	$query = $this->get('db')->prepare("SELECT * FROM users WHERE username=?");
+    	$query->bindParam(1, $args['username']);
+    	$query->execute();
+
+    	$user = $query->fetch();
+
+    	$result = [
+    		'status' => !$user ? false : true,
+    		'data' => []
+    	];
+
+    	if($user)
+    		$result['data'] = $user;
+    	else
+    		$result['data']['message'] = 'User tidak ditemukan';
+
+    	$response->getBody()->write(json_encode($result));
+
+        return $response
+        	->withHeader('Content-Type', 'application/json');
+        	
+    });
+
 });
