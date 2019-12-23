1e8c3d8d0d406d7ae2bfc76ced7acd9c049e16c5
Satria Aji Putra <satmaxt@outlook.com>
2019-12-23 21:15:15 +0700

    Create: LoginActivity

--- app/src/main/java/my/id/satria/safarea/MainActivity.java
+++ app/src/main/java/my/id/satria/safarea/MainActivity.java
@@ -1,14 +1,28 @@
 package my.id.satria.safarea;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(l -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
     }
 }
