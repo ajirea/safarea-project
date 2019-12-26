ed8ad91d6024dbaf34985d8ee92e0a77f607e903
Satria Aji Putra <satmaxt@outlook.com>
2019-12-26 13:58:28 +0700

    Move database location to /api

--- api/safarea.sql
+++ api/safarea.sql
@@ -1,144 +1,325 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2019-12-18 12:48:38.127
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2019 at 02:22 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
--- tables
--- Table: addresses
-CREATE TABLE addresses (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    users_id int unsigned NULL,
-    buyers_id int unsigned NULL,
-    address varchar(255) NOT NULL COMMENT 'Address line 1',
-    village varchar(30) NOT NULL,
-    district varchar(30) NOT NULL,
-    city varchar(30) NOT NULL,
-    province varchar(30) NOT NULL,
-    postal_code varchar(7) NOT NULL,
-    CONSTRAINT addresses_pk PRIMARY KEY (id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Table: api_tokens
-CREATE TABLE api_tokens (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user_id int unsigned NOT NULL,
-    token int unsigned NOT NULL,
-    expired_at timestamp NOT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT api_tokens_pk PRIMARY KEY (id)
-);
 
--- Table: buyers
-CREATE TABLE buyers (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user_id int unsigned NOT NULL,
-    name varchar(70) NOT NULL,
-    phone varchar(20) NOT NULL,
-    deleted_at timestamp NULL,
-    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT buyers_pk PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table: orders
-CREATE TABLE orders (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    user_id int unsigned NOT NULL,
-    buyer_id int unsigned NOT NULL,
-    order_number varchar(50) NOT NULL,
-    price decimal(12,2) NOT NULL,
-    profit_price decimal(12,2) NOT NULL,
-    qty int NOT NULL,
-    total decimal(12,2) NOT NULL COMMENT '(price+profit_price)*qty',
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE INDEX orders_ak_1 (order_number),
-    CONSTRAINT orders_pk PRIMARY KEY (id)
-);
+--
+-- Database: `safarea`
+--
 
--- Table: product_images
-CREATE TABLE product_images (
-    id int unsigned NOT NULL,
-    product_id int unsigned NOT NULL,
-    name varchar(120) NOT NULL,
-    path varchar(255) NOT NULL,
-    created_at timestamp NOT NULL,
-    CONSTRAINT product_images_pk PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
--- Table: products
-CREATE TABLE products (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    name varchar(120) NOT NULL,
-    slug varchar(120) NOT NULL,
-    thumbnail varchar(255) NOT NULL,
-    price decimal(12,2) NOT NULL,
-    stock int NOT NULL,
-    description text NOT NULL,
-    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE INDEX products_ak_1 (slug),
-    CONSTRAINT products_pk PRIMARY KEY (id)
-);
+--
+-- Table structure for table `addresses`
+--
 
--- Table: user_products
-CREATE TABLE user_products (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    product_id int unsigned NOT NULL,
-    user_id int unsigned NOT NULL,
-    profit_price decimal(12,2) NOT NULL,
-    qty int NOT NULL DEFAULT 0,
-    status varchar(15) NOT NULL COMMENT '''''take'''', ''''sending'''', ''''active'''', ''''non-active'''', ''''returning''''',
-    CONSTRAINT user_products_pk PRIMARY KEY (id)
-);
+CREATE TABLE `addresses` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `users_id` int(10) UNSIGNED DEFAULT NULL,
+  `buyers_id` int(10) UNSIGNED DEFAULT NULL,
+  `address` varchar(255) NOT NULL COMMENT 'Address line 1',
+  `village` varchar(30) NOT NULL,
+  `district` varchar(30) NOT NULL,
+  `city` varchar(30) NOT NULL,
+  `province` varchar(30) NOT NULL,
+  `postal_code` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Table: users
-CREATE TABLE users (
-    id int unsigned NOT NULL AUTO_INCREMENT,
-    username varchar(16) NOT NULL,
-    email varchar(120) NOT NULL,
-    name varchar(70) NOT NULL,
-    password varchar(120) NOT NULL,
-    store_name varchar(60) NOT NULL,
-    phone varchar(20) NOT NULL,
-    avatar varchar(255) NULL,
-    is_admin bool NOT NULL DEFAULT false,
-    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE INDEX users_ak_1 (username,email,store_name,phone),
-    CONSTRAINT users_pk PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
--- foreign keys
--- Reference: addresses_buyers (table: addresses)
-ALTER TABLE addresses ADD CONSTRAINT addresses_buyers FOREIGN KEY addresses_buyers (buyers_id)
-    REFERENCES buyers (id);
+--
+-- Table structure for table `api_tokens`
+--
 
--- Reference: addresses_users (table: addresses)
-ALTER TABLE addresses ADD CONSTRAINT addresses_users FOREIGN KEY addresses_users (users_id)
-    REFERENCES users (id);
+CREATE TABLE `api_tokens` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `token` varchar(16) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Reference: api_tokens_users (table: api_tokens)
-ALTER TABLE api_tokens ADD CONSTRAINT api_tokens_users FOREIGN KEY api_tokens_users (user_id)
-    REFERENCES users (id);
+--
+-- Dumping data for table `api_tokens`
+--
 
--- Reference: buyers_users (table: buyers)
-ALTER TABLE buyers ADD CONSTRAINT buyers_users FOREIGN KEY buyers_users (user_id)
-    REFERENCES users (id);
+INSERT INTO `api_tokens` (`id`, `user_id`, `token`, `created_at`) VALUES
+(4, 13, '3Ogvk5Y26ZYIygPY', '2019-12-24 10:04:01');
 
--- Reference: orders_buyers (table: orders)
-ALTER TABLE orders ADD CONSTRAINT orders_buyers FOREIGN KEY orders_buyers (buyer_id)
-    REFERENCES buyers (id);
+-- --------------------------------------------------------
 
--- Reference: orders_users (table: orders)
-ALTER TABLE orders ADD CONSTRAINT orders_users FOREIGN KEY orders_users (user_id)
-    REFERENCES users (id);
+--
+-- Table structure for table `buyers`
+--
 
--- Reference: product_images_products (table: product_images)
-ALTER TABLE product_images ADD CONSTRAINT product_images_products FOREIGN KEY product_images_products (product_id)
-    REFERENCES products (id);
+CREATE TABLE `buyers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(70) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Reference: user_products_products (table: user_products)
-ALTER TABLE user_products ADD CONSTRAINT user_products_products FOREIGN KEY user_products_products (product_id)
-    REFERENCES products (id);
+-- --------------------------------------------------------
 
--- Reference: user_products_users (table: user_products)
-ALTER TABLE user_products ADD CONSTRAINT user_products_users FOREIGN KEY user_products_users (user_id)
-    REFERENCES users (id);
+--
+-- Table structure for table `orders`
+--
 
--- End of file.
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `buyer_id` int(10) UNSIGNED NOT NULL,
+  `order_number` varchar(50) NOT NULL,
+  `price` decimal(12,2) NOT NULL,
+  `profit_price` decimal(12,2) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total` decimal(12,2) NOT NULL COMMENT '(price+profit_price)*qty',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `slug` varchar(120) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `price` decimal(12,2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(120) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  `name` varchar(70) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `store_name` varchar(60) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `name`, `password`, `store_name`, `phone`, `avatar`, `is_admin`, `created_at`) VALUES
+(8, 'asha', 'ashanaufal1@gmail.com', 'Naufal', '$2y$10$2ht0Z4gw3nTkydDde6.1ge0/iEFmhms029MtYRZL2X0Fqa7nQ4izS', 'Naufal Store 2', '088899922222', 'aang1.png', 0, '2019-12-24 04:37:20'),
+(13, 'ashar', 'ashanaufal12@gmail.com', 'NaufalA!', '$2y$10$0WeIn2Bes0rLZFm5WleN0.Qmm3kTeB484U9EZAAHgw2z2v.nE/gKi', 'Naufal Store 2', '088899922222', 'aang1.png', 0, '2019-12-24 10:04:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_products`
+--
+
+CREATE TABLE `user_products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `profit_price` decimal(12,2) NOT NULL,
+  `qty` int(11) NOT NULL DEFAULT 0,
+  `status` varchar(15) NOT NULL COMMENT '''''take'''', ''''sending'''', ''''active'''', ''''non-active'''', ''''returning'''''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `addresses_buyers` (`buyers_id`),
+  ADD KEY `addresses_users` (`users_id`);
+
+--
+-- Indexes for table `api_tokens`
+--
+ALTER TABLE `api_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `api_tokens_users` (`user_id`);
+
+--
+-- Indexes for table `buyers`
+--
+ALTER TABLE `buyers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `buyers_users` (`user_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `orders_ak_1` (`order_number`),
+  ADD KEY `orders_buyers` (`buyer_id`),
+  ADD KEY `orders_users` (`user_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `products_ak_1` (`slug`);
+
+--
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_images_products` (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_ak_1` (`username`,`email`,`store_name`,`phone`);
+
+--
+-- Indexes for table `user_products`
+--
+ALTER TABLE `user_products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_products_products` (`product_id`),
+  ADD KEY `user_products_users` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `api_tokens`
+--
+ALTER TABLE `api_tokens`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `buyers`
+--
+ALTER TABLE `buyers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_products`
+--
+ALTER TABLE `user_products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_buyers` FOREIGN KEY (`buyers_id`) REFERENCES `buyers` (`id`),
+  ADD CONSTRAINT `addresses_users` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `api_tokens`
+--
+ALTER TABLE `api_tokens`
+  ADD CONSTRAINT `api_tokens_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `buyers`
+--
+ALTER TABLE `buyers`
+  ADD CONSTRAINT `buyers_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_buyers` FOREIGN KEY (`buyer_id`) REFERENCES `buyers` (`id`),
+  ADD CONSTRAINT `orders_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD CONSTRAINT `product_images_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `user_products`
+--
+ALTER TABLE `user_products`
+  ADD CONSTRAINT `user_products_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `user_products_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
