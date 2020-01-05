ca55e60e0457ac465d7b86dea54117232f179867
Satria Aji Putra <satmaxt@outlook.com>
2020-01-05 10:17:57 +0700

    Delete unused files

--- app/src/main/java/my/id/satria/safarea/repositories/ServerAPI.java
+++
@@ -1,8 +0,0 @@
-package my.id.satria.safarea.repositories;
-
-public class ServerAPI {
-
-    public static final String BASE_URL = "https://api.satmaxt.xyz/safarea";
-    public static final String API_URL = BASE_URL.concat("/api/");
-    public static final String AUTH_LOGIN = API_URL.concat("auth/login");
-}
