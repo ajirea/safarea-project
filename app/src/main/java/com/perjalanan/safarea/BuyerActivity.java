b4c12ba9bcb695b53463fe1d35bee84a52d0ed1b
Satria Aji Putra <satmaxt@outlook.com>
2020-01-06 12:31:06 +0700

    API Implementation: Buyer list

--- app/src/main/java/com/perjalanan/safarea/BuyerActivity.java
+++ app/src/main/java/com/perjalanan/safarea/BuyerActivity.java
@@ -1,22 +1,45 @@
 package com.perjalanan.safarea;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.perjalanan.safarea.adapters.BuyerListAdapter;
 import com.perjalanan.safarea.data.BuyerItem;
+import com.perjalanan.safarea.data.User;
 import com.perjalanan.safarea.helpers.ToolbarHelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.perjalanan.safarea.repositories.RequestGlobalHeaders;
+import com.perjalanan.safarea.repositories.ServerAPI;
+import com.perjalanan.safarea.repositories.UserLocalStore;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class BuyerActivity extends AppCompatActivity {
 
     private ToolbarHelper toolbarHelper;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private User user;
+    private ArrayList<BuyerItem> buyerList;
+    private RequestQueue requestQueue;
+    private UserLocalStore userLocalStore;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -31,10 +54,26 @@ public class BuyerActivity extends AppCompatActivity {
         toolbarHelper.initToolbar(true);
         toolbarHelper.setToolbarTitle(getString(R.string.text_buyer_lists));
 
+        // mengambil data user yang login
+        userLocalStore = new UserLocalStore(this);
+        user = userLocalStore.getLoggedInUser();
+
+        // inisiasi request queue
+        requestQueue = Volley.newRequestQueue(this);
+
+        // inisiasi data
+        buyerList = new ArrayList<>();
+        getBuyers();
+
+        // component
+        swipeRefreshLayout = findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(this::getBuyers);
+
         // recyclerview buyer
         mRecyclerView = findViewById(R.id.buyerListRecyclerView);
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new BuyerListAdapter(exampleBuyerData());
+        mAdapter = new BuyerListAdapter(buyerList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
@@ -67,5 +106,55 @@ public class BuyerActivity extends AppCompatActivity {
         exBuyers.add(buyer2);
 
         return exBuyers;
+    }
+
+    /**
+     * Method untuk mengambil data pembeli dari API
+     */
+    private void getBuyers() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                .setTitle("Error!");
+
+        String url = ServerAPI.BUYER_ALL_DATA + "/" + user.getId();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        if(response.getBoolean("status")) {
+                            buyerList.clear();
+                            for(int i = 0; i < response.getJSONArray("data").length(); i++) {
+                                JSONObject item = response.getJSONArray("data").getJSONObject(i);
+                                BuyerItem buyer = new BuyerItem(
+                                        Integer.parseInt(item.getString("id")),
+                                        Integer.parseInt(item.getString("user_id")),
+                                        item.getString("name"),
+                                        item.getString("phone")
+                                );
+                                buyer.setCreatedAt(item.getString("created_at"));
+                                buyer.setDeletedAt(item.getString("deleted_at"));
+                                buyerList.add(buyer);
+                            }
+                            ProgressBar progress = findViewById(R.id.progress_circular);
+                            progress.setVisibility(View.INVISIBLE);
+                            mAdapter.notifyDataSetChanged();
+
+                        } else {
+                            alert.setMessage(response.getJSONObject("data")
+                                    .getString("message")).show();
+                        }
+                        swipeRefreshLayout.setRefreshing(false);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        alert.setMessage(e.getMessage()).show();
+                    }
+                }, error -> {
+            alert.setMessage(error.getMessage()).show();
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return RequestGlobalHeaders.get(getApplicationContext());
+            }
+        };
+
+        requestQueue.add(request);
     }
 }
--- app/src/main/java/com/perjalanan/safarea/repositories/ServerAPI.java
+++ app/src/main/java/com/perjalanan/safarea/repositories/ServerAPI.java
@@ -9,4 +9,6 @@ public class ServerAPI {
     public static final String CHANGE_PASSWORD_ACCOUNT = API_URL.concat("user/change-password");
     public static final String CHANGE_ADDRESS_ACCOUNT = API_URL.concat("user/address");
 
+    public static final String BUYER_ALL_DATA = API_URL.concat("buyers");
+
 }
